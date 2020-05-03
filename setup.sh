30fbbb8f3b1abc41bce10ffe3a0d3d19d13bcff9
Alex Botelho <botelho2305@gmail.com>
2020-05-03 19:33:00 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,49 @@
+
 #!/bin/bash
+echo "Installing necessary packages..."
+sudo apt -y install realmd libnss-sss libpam-sss sssd sssd-tools adcli samba-common-bin oddjob oddjob-mkhomedir packagekit -y
+echo "Installed necessary packages."
+echo "Printing discovered Kerberos realms..."
+realm discover
 
-echo "Running step 1..."
-sudo bash step1.sh
+echo "Your domain and its properties should be printed above. If they are not, check DNS config."
+read -p "What is the Kerberos realm? (dom.example.com)? " realmad
+read -p "What is the domain controllers short hostname ? ('dc' part of dc.dom.example.com)? " realdc
+shorthost = ${HOSTNAME%%.*}
 
-echo "Running step 2..."
-sudo bash step2.sh
+mkdir /etc/univention
+ssh -n root@"$realdc.$realad" 'ucr shell | grep -v ^hostname=' >/etc/univention/ucr_master
+echo "master_ip="$realdc.$realad"" >>/etc/univention/ucr_master
+chmod 660 /etc/univention/ucr_master
 
+# Create an account and save the password
+password="$(tr -dc A-Za-z0-9_ </dev/urandom | head -c20)"
+ssh -n root@"$realdc.$realad" udm computers/ubuntu create \
+    --position "cn=computers,${ldap_base}" \
+    --set name=$(hostname) --set password="${password}" \
+    --set operatingSystem="$(lsb_release -is)" \
+    --set operatingSystemVersion="$(lsb_release -rs)"
+printf '%s' "$password" >/etc/ldap.secret
+chmod 0400 /etc/ldap.secret
 
-echo "Running step 3..."
-sudo bash step3.sh
+# Create ldap.conf
+echo 'TLS_CACERT /etc/univention/ssl/ucsCA/CAcert.pem
+URI ldap://$ldap_master:7389
+BASE $ldap_base' | sudo tee /etc/ldap/ldap.conf
 
-echo "Integration into domain complete."
+echo "Printing available Kerberos realms. If AD does not appear, exit now and resolve DNS issues."
+read -p "What is the AD domain/realm? " adrealm
+read -p "What is the domain admin username? " aduser
+
+sudo realm join -v -U "$aduser" "$adrealm"
+
+echo "Ensure activate mkhomedir is selected in the next prompt."
+
+echo 'Name: activate mkhomedir
+Default: yes
+Priority: 900
+Session-Type: Additional
+Session:
+        required  pam_mkhomedir.so umask=0022 skel=/etc/skel' | sudo tee /usr/share/pam-configs/mkhomedir
+sudo pam-auth-update -f
+sudo systemctl restart sssd
