cf962e32d0d46084edea58e7d2b3855f748bda95
Alex Botelho <botelho2305@gmail.com>
2020-05-03 17:21:58 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+
+echo "Running step 1..."
 sudo bash step1.sh
 
+echo "Running step 2..."
 sudo bash step2.sh
 
-echo "Integration in domain complete."
+
+echo "Running step 3..."
+sudo bash step3.sh
+
+echo "Integration into domain complete."
