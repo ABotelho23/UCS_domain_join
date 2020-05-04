fdce7973f0e62f695e8bde0c744486c7a11b379c
Alex Botelho <botelho2305@gmail.com>
2020-05-03 20:19:03 -0400

    Little bug fixes

--- setup.sh
+++ setup.sh
@@ -13,17 +13,17 @@ read -p "What is the domain admin username? " REALMADMIN
 shorthost=${HOSTNAME%%.*}
 
 mkdir /etc/univention
-echo "Connecting to $REALMDC.$REALMAD UCS server and pulling UCS config."
-ssh -n root@$REALMDC.$REALMAD 'ucr shell | grep -v ^hostname=' >/etc/univention/ucr_master
-echo "master_ip=$REALMDC.$REALMAD" >>/etc/univention/ucr_master
+echo "Connecting to "$REALMDC.$REALMAD" UCS server and pulling UCS config."
+ssh -n $REALMADMIN@$REALMDC.$REALMAD 'ucr shell | grep -v ^hostname=' >/etc/univention/ucr_master
+echo "master_ip="$REALMDC.$REALMAD"" >>/etc/univention/ucr_master
 chmod 660 /etc/univention/ucr_master
 
 . /etc/univention/ucr_master
 
 # Create an account and save the password
-echo "Creating computer account on "$realdc.$realad" UCS server. Connecting..."
+echo "Creating computer account on "$REALMDC.$REALMAD" UCS server. Connecting..."
 password="$(tr -dc A-Za-z0-9_ </dev/urandom | head -c20)"
-ssh -n root@$REALMDC.$REALMAD udm computers/ubuntu create \
+ssh -n $REALMADMIN@$REALMDC.$REALMAD udm computers/ubuntu create \
     --position "cn=computers,${ldap_base}" \
     --set name=$(hostname) --set password="${password}" \
     --set operatingSystem="$(lsb_release -is)" \
@@ -39,7 +39,7 @@ echo 'TLS_CACERT /etc/univention/ssl/ucsCA/CAcert.pem
 URI ldap://$ldap_master:7389
 BASE $ldap_base' | sudo tee /etc/ldap/ldap.conf
 
-read -p "Ensure activate mkhomedir is selected using your arrow keys and space bar in the next prompt. Press ANY KEY to continue."
+read -p "Ensure 'activate mkhomedir' is selected using your arrow keys and space bar in the next prompt. Press ANY KEY to continue."
 echo "Ensure activate mkhomedir is selected using your arrow keys and space bar in the next prompt."
 
 echo 'Name: activate mkhomedir
@@ -48,7 +48,7 @@ Priority: 900
 Session-Type: Additional
 Session:
         required  pam_mkhomedir.so umask=0022 skel=/etc/skel' | sudo tee /usr/share/pam-configs/mkhomedir
-DEBIAN_FRONTEND=noninteractive pam-auth-update --force
+sudo pam-auth-update
 sudo systemctl restart sssd
 
 #prompt
