6a7b94961a72c8ed506806f311ee6711b54591d0
Alex Botelho <botelho2305@gmail.com>
2020-05-03 20:27:03 -0400

    fix issue where udm/ucr command couldn't run

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ shorthost=${HOSTNAME%%.*}
 
 mkdir /etc/univention
 echo "Connecting to "$REALMDC.$REALMAD" UCS server and pulling UCS config. Password for domain admin will be prompted."
-ssh -n $REALMADMIN@$REALMDC.$REALMAD 'ucr shell | grep -v ^hostname=' >/etc/univention/ucr_master
+ssh -n root@$REALMDC.$REALMAD 'ucr shell | grep -v ^hostname=' >/etc/univention/ucr_master
 echo "master_ip="$REALMDC.$REALMAD"" >>/etc/univention/ucr_master
 chmod 660 /etc/univention/ucr_master
 
@@ -23,7 +23,7 @@ chmod 660 /etc/univention/ucr_master
 # Create an account and save the password
 echo "Creating computer account on "$REALMDC.$REALMAD" UCS server. Password for domain admin will be prompted."
 password="$(tr -dc A-Za-z0-9_ </dev/urandom | head -c20)"
-ssh -n $REALMADMIN@$REALMDC.$REALMAD udm computers/ubuntu create \
+ssh -n root@$REALMDC.$REALMAD udm computers/ubuntu create \
     --position "cn=computers,${ldap_base}" \
     --set name=$(hostname) --set password="${password}" \
     --set operatingSystem="$(lsb_release -is)" \
